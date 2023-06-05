b13ec859c2b1219e304dfe0a94291cf795e34646
Chenggg95 <wenjie73205@gmail.com>
2023-06-05 21:44:07 +0800

    Third commit

--- src/index.js
+++ src/index.js
@@ -34,13 +34,12 @@ pool.connect((error, client, release) => {
     })
 }) 
 
-app.post('/savetransaction', (req, res, next) => {
+app.post('/savetransaction', (req, res) => {
     const fruitList = req.body.transaction.fruitList;
     const total = req.body.transaction.total;
 
     console.log("saving data");
     const insertQuery = 'INSERT INTO FRUIT_TRANSACTION (fruit_list,total) VALUES ($1::jsonb, $2::numeric)';
-
     const values = [JSON.stringify(fruitList), total];
 
     pool.query(insertQuery, values, (err, result) => {
@@ -52,6 +51,48 @@ app.post('/savetransaction', (req, res, next) => {
     console.log('Transaction inserted successfully');
     res.sendStatus(200);
     })
+})
+
+app.get('/getstock', (req, res) => {
+    const getQuery = 'SELECT * FROM FRUIT_STOCK';
+
+    pool.query(getQuery, (err, result) => {
+        if(err){
+            console.error('Error getting fruits stock', err);
+            res.sendStatus(500);
+            return;
+        }
+        res.json(result.rows);
+    })
+})
+
+app.put('/updatestock', (req, res) => {
+    const putQuery = 'UPDATE fruit_stock SET quantity=$1::integer WHERE fruit=$2::varchar(255)';
+    const updates = req.body.update;
+
+    let completedUpdates = 0;
+    let errorOccurred = false;
+
+    updates.forEach(update => {
+        const values = [update.quantity, update.fruit];
+        pool.query(putQuery, values, (err, result) => {
+            if(err){
+                console.error('Error updating fruit stock', err);
+                errorOccurred = true;
+            }
+            completedUpdates++;
+            if(completedUpdates === updates.length){
+                if(errorOccurred){
+                    res.sendStatus(500);
+                }
+                else{
+                    console.log('Updated');
+                    res.sendStatus(200);
+                }
+            }
+        })
+    })
+    
 })
 
 const server = app.listen(3001, function(){
